50cd9b660b1d7293b5c61e4da46210a1c4ab2047
llalon <71791017+llalon@users.noreply.github.com>
2022-08-06 14:39:05 -0400

    Update Util.php

--- source/composerize/usr/local/emhttp/plugins/composerize/include/Util.php
+++ source/composerize/usr/local/emhttp/plugins/composerize/include/Util.php
@@ -6,18 +6,11 @@ require_once("$docroot/plugins/composerize/include/Definitions.php");
 
 function debugToConsole($data)
 {
-    $output = clean($data);
+    $output = preg_replace('/[^A-Za-z0-9\-]/', '', $data); // Removes special chars.;
     if (is_array($output))
         $output = implode(',', $output);
 
     echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
-}
-
-function clean($string)
-{
-    $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
-
-    return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
 }
 
 function checkDependencies(): bool
