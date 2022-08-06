ab83a7f7d2adb17cb45614d28e64657c90955b95
llalon <71791017+llalon@users.noreply.github.com>
2022-08-06 15:26:29 -0400

    Update

--- source/composerize/usr/local/emhttp/plugins/composerize/include/Util.php
+++ source/composerize/usr/local/emhttp/plugins/composerize/include/Util.php
@@ -4,15 +4,6 @@ $docroot = $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
 
 require_once("$docroot/plugins/composerize/include/Definitions.php");
 
-function debugToConsole($data)
-{
-    $output = preg_replace('/[^A-Za-z0-9\-]/', '', $data); // Removes special chars.;
-    if (is_array($output))
-        $output = implode(',', $output);
-
-    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
-}
-
 function checkDependencies(): bool
 {
     // Check if compose.manager plugin is installed.
